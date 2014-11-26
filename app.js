ceac3fbe40fd98b785260a63fe553a8f9099b784
Luis Leao <luis.leao@gmail.com>
2014-11-26 14:42:44 -0200

    node urls

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 var express = require('express')
 var app = express();
 
+
+
+
+var home_handler = function(request, response) {
+	response.send('Hello World!')
+};
+
+var call_handler = function(request, response) {};
+
+var newrecord_handler = function(request, response) {};
+
+var records_handler = function(request, response) {};
+
+
+
+
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/', home_handler);
+app.get('/call', call_handler);
+app.get('/newrecord', newrecord_handler);
+app.get('/records', records_handler);
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
